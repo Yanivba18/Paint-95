3516dcb25a2cf9efe0107e3e14b2e73bb7af9c36
Yanivba18 <47143949+Yanivba18@users.noreply.github.com>
2019-11-19 00:18:38 +0200

    created main logic of drawing

---
+++ js/myscript.js
@@ -0,0 +1,30 @@
+"use strict";
+(() => {
+    let canvas = document.getElementById("canvas");
+    
+    let main = () => {
+        addListeners();        
+    }
+    
+    let addListeners = () => {
+        canvas.addEventListener("mousedown", (ev) => {
+            draw(ev);
+            let drawFunc = (e) => {draw(e)};
+            canvas.addEventListener("mousemove", drawFunc);
+            canvas.addEventListener("mouseup", () => {canvas.removeEventListener("mousemove", drawFunc)});
+        }); 
+    }
+
+    let draw = (eVar) => {
+        let x = eVar.clientX;
+        let y = eVar.clientY;
+        let box = document.createElement("div");
+        box.className = "box-draw black";
+        box.style.top = `${y}px`;
+        box.style.left = `${x}px`;
+        canvas.append(box);
+    }
+
+    main();
+
+})(window)
