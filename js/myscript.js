5be38fd241194d7cc9dc3cb77a8cd36d52def6ba
Yanivba18 <47143949+Yanivba18@users.noreply.github.com>
2019-11-20 00:06:30 +0200

    Added functionality of draw size

--- js/myscript.js
+++ js/myscript.js
@@ -1,29 +1,54 @@
 "use strict";
 (() => {
     let canvas = document.getElementById("canvas");
-    
-    let main = () => {
-        addListeners();        
+    let canvasPosition = canvas.getBoundingClientRect();
+    var color = "black";
+    let getBoxCoordinates = {
+        top: canvasPosition.top,
+        bottom: canvasPosition.bottom,
+        left: canvasPosition.left,
+        right: canvasPosition.right
     }
-    
+
+    let main = () => {
+        addListeners();
+    }
+
     let addListeners = () => {
         canvas.addEventListener("mousedown", (ev) => {
             draw(ev);
-            let drawFunc = (e) => {draw(e)};
+            let drawFunc = (e) => { draw(e) };
             canvas.addEventListener("mousemove", drawFunc);
-            document.body.addEventListener("mouseup", () => {canvas.removeEventListener("mousemove", drawFunc)});
-        }); 
+            document.body.addEventListener("mouseup", () => { canvas.removeEventListener("mousemove", drawFunc) });
+        });
+        document.querySelectorAll(".dot").addEventListener("click", )
+    }
+
+    let getDrawSize = () => {
+        let size = document.getElementById("size");
+        return size.value + "px";
     }
 
     let draw = (eVar) => {
         let x = eVar.clientX;
         let y = eVar.clientY;
         let box = document.createElement("div");
-        box.className = "box-draw black";
+        box.className = `box-draw ${color}`;
         box.style.top = `${y}px`;
         box.style.left = `${x}px`;
-        canvas.append(box);
+        box.style.height = getDrawSize();
+        box.style.width = getDrawSize();        
+        reachedEndOfCanvas(x, y) ? null : canvas.append(box);
+
     }
+
+    let reachedEndOfCanvas = (x, y) => {
+        let drawSize = parseInt(getDrawSize());
+        return (x + drawSize > getBoxCoordinates.right) ? true :
+            (y + drawSize > getBoxCoordinates.bottom) ? true :
+                false;
+    }
+
 
     main();
 
