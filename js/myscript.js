9c64edf6b2936a6aba581702a723ee2b851156d3
Yanivba18 <47143949+Yanivba18@users.noreply.github.com>
2019-11-21 11:31:01 +0200

    added functions to get canvas pos and getting the boxes. Added erase function

--- js/myscript.js
+++ js/myscript.js
@@ -1,14 +1,8 @@
 "use strict";
 (() => {
-    let canvas = document.getElementById("canvas");
-    let canvasPosition = canvas.getBoundingClientRect();
+    let canvas = document.getElementById("canvas");    
     let color = "black";
-    let getBoxCoordinates = {
-        top: canvasPosition.top,
-        bottom: canvasPosition.bottom,
-        left: canvasPosition.left,
-        right: canvasPosition.right
-    }
+
 
     let main = () => {
         addListeners();
@@ -22,8 +16,44 @@
             document.body.addEventListener("mouseup", () => { canvas.removeEventListener("mousemove", drawFunc) });
         });
         let dots = $('dot');
-        dots.click(() => {color = event.target.id}) //addListeners("click", () => {color = event.target.id})
-        //document.querySelectorAll("dot").addEventListener("click", () => {color = event.target.id});
+        dots.click(() => { color = event.target.id });
+
+        let btn = $('#erase');
+        btn.click(function () {
+            while (canvas.firstChild) {
+                canvas.removeChild(canvas.firstChild);
+            }
+        })
+
+        $(window).resize(function() {
+            let divs = getDrawnBoxes();            
+            let rectPos = getRectSize();
+            console.log("resize event works!")
+            for (let i = 0; i < divs.length; i++) {
+                let divPos = divs.eq(i).offset();
+                // console.log(divPos);
+                if (divPos.top < rectPos.top || divPos.top > rectPos.bottom || divPos.left < rectPos.left || divPos.left > rectPos.right) {
+                    canvas.removeChild(divs.eq(i));
+                }
+            }
+        })
+    }
+
+    function getDrawnBoxes() {
+        let divs = $('.box-draw');            
+        return divs;
+    }
+
+    function getRectSize() {
+        let canvasPosition = canvas.getBoundingClientRect();
+        let getBoxCoordinates = {
+            top: canvasPosition.top,
+            bottom: canvasPosition.bottom,
+            left: canvasPosition.left,
+            right: canvasPosition.right
+        }
+        console.log(`getting rect size: ${canvasPosition}`)
+        return getBoxCoordinates;
     }
 
     let getDrawSize = () => {
@@ -39,16 +69,19 @@
         box.style.top = `${y}px`;
         box.style.left = `${x}px`;
         box.style.height = getDrawSize();
-        box.style.width = getDrawSize();        
+        box.style.width = getDrawSize();
         reachedEndOfCanvas(x, y) ? null : canvas.append(box);
 
     }
 
     let reachedEndOfCanvas = (x, y) => {
         let drawSize = parseInt(getDrawSize());
-        return (x + drawSize > getBoxCoordinates.right) ? true :
-            (y + drawSize > getBoxCoordinates.bottom) ? true :
-                false;
+        let rectSize = getRectSize();
+        return (x + drawSize > rectSize.right) ? true :
+            (y + drawSize > rectSize.bottom) ? true :
+                (x < rectSize.left) ? true :
+                    (y < rectSize.top) ? true :
+                        false;
     }
 
 
