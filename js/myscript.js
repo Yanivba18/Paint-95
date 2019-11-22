9d2919e66a4df2cc11a3bbe7e7ceb3647fcd813d
Yanivba18 <47143949+Yanivba18@users.noreply.github.com>
2019-11-22 23:19:39 +0200

    Added eraser, setting canvas size, canvas slider has auto max

--- js/myscript.js
+++ js/myscript.js
@@ -1,42 +1,72 @@
 "use strict";
 (() => {
-    let canvas = document.getElementById("canvas");    
+    let canvas = document.getElementById("canvas");
     let color = "black";
 
 
     let main = () => {
         addListeners();
+        setCanvasSize();
+        setCanvasSliderMax();
     }
 
     let addListeners = () => {
         canvas.addEventListener("mousedown", (ev) => {
-            draw(ev);
-            let drawFunc = (e) => { draw(e) };
-            canvas.addEventListener("mousemove", drawFunc);
-            document.body.addEventListener("mouseup", () => { canvas.removeEventListener("mousemove", drawFunc) });
+            if (ev.button == 0) {
+                draw(ev);
+                let drawFunc = (e) => { draw(e) };
+                canvas.addEventListener("mousemove", drawFunc);
+                document.body.addEventListener("mouseup", () => { canvas.removeEventListener("mousemove", drawFunc) });
+            }
         });
         let dots = $('dot');
         dots.click(() => { color = event.target.id });
 
-        let btn = $('#erase');
-        btn.click(function () {
+        let $btn = $('#clear');
+        $btn.click(function () {
             while (canvas.firstChild) {
                 canvas.removeChild(canvas.firstChild);
-            }
-        })
+            }});
+        let $btnEraser = $('#eraser');
+        $btnEraser.click(function() { color = "white"});
+        $(window).resize(function() {hideOutOfCanvas(); setCanvasSliderMax();});
+        $('#canvasSize').on('input', setCanvasSize);
 
-        $(window).resize(function() {
-            let divs = getDrawnBoxes();            
-            let rectPos = getRectSize();
-            console.log("resize event works!")
-            for (let i = 0; i < divs.length; i++) {
-                let divPos = divs.eq(i).offset();
-                // console.log(divPos);
-                if (divPos.top < rectPos.top || divPos.top > rectPos.bottom || divPos.left < rectPos.left || divPos.left > rectPos.right) {
-                    canvas.removeChild(divs.eq(i));
-                }
+    }
+
+    function setCanvasSliderMax() {
+        let $slider = $('#canvasSize');
+        $slider.attr('max', () => {return $(window).width() > $(window).height() ? $(window).width() - 160 : $(window).height() - 50;})
+    }
+
+    function setCanvasSize() {
+        let $canvasSlider = $('#canvasSize');
+        let newCanvasSize = parseInt($canvasSlider.val());
+        let currentPos = getCanvasPos();
+        
+        if ((currentPos.top + newCanvasSize) < ($(window).height() - 5)) { 
+            canvas.style.height = `${newCanvasSize}px`;
+        } 
+        if ((currentPos.left + newCanvasSize) < ($(window).width() -5)) {
+            canvas.style.width = `${newCanvasSize}px`;
+        }
+
+        hideOutOfCanvas();
+    }
+
+    function hideOutOfCanvas() {
+        let divs = getDrawnBoxes();            
+        let rectPos = getCanvasPos();
+        console.log("resize event works!")
+        for (let i = 0; i < divs.length; i++) {
+            let divPos = divs.eq(i).offset();
+            let divSize = divs.eq(i).width();
+            if (divPos.top < rectPos.top || (divPos.top + divSize) > rectPos.bottom || divPos.left < rectPos.left || (divPos.left + divSize) > rectPos.right) {
+                divs.eq(i).css('visibility', 'hidden');
+            } else {
+                divs.eq(i).css('visibility', 'visible');
             }
-        })
+        }
     }
 
     function getDrawnBoxes() {
@@ -44,16 +74,10 @@
         return divs;
     }
 
-    function getRectSize() {
-        let canvasPosition = canvas.getBoundingClientRect();
-        let getBoxCoordinates = {
-            top: canvasPosition.top,
-            bottom: canvasPosition.bottom,
-            left: canvasPosition.left,
-            right: canvasPosition.right
-        }
+    function getCanvasPos() {
+        let canvasPosition = canvas.getBoundingClientRect();         
         console.log(`getting rect size: ${canvasPosition}`)
-        return getBoxCoordinates;
+        return canvasPosition;
     }
 
     let getDrawSize = () => {
@@ -76,7 +100,7 @@
 
     let reachedEndOfCanvas = (x, y) => {
         let drawSize = parseInt(getDrawSize());
-        let rectSize = getRectSize();
+        let rectSize = getCanvasPos();
         return (x + drawSize > rectSize.right) ? true :
             (y + drawSize > rectSize.bottom) ? true :
                 (x < rectSize.left) ? true :
